8ab2ef136af8cc98d73539f93dc6941364045ff6
Frank Pinto <frank.joseph.pinto@gmail.com>
2013-06-10 18:36:37 -0400

    Add minimal functionality for blocking user agents

--- lib/Analytics.php
+++ lib/Analytics.php
@@ -9,6 +9,13 @@ require(dirname(__FILE__) . '/Analytics/Client.php');
 
 class Analytics {
 
+  public $blocked_uas = array(
+      'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',
+      'DoCoMo/2.0 N905i(c100;TB;W24H16) (compatible; Googlebot-Mobile/2.1; +http://www.google.com/bot.html)',
+      'Mozilla/5.0 (compatible; MJ12bot/v1.4.3; http://www.majestic12.co.uk/bot.php?+)',
+      'SAMSUNG-SGH-E250/1.0 Profile/MIDP-2.0 Configuration/CLDC-1.1 UP.Browser/6.2.3.3.c.1.101 (GUI) MMP/2.0 (compatible; Googlebot-Mobile/2.1; +http://www.google.com/bot.html)'
+    );
+  
   private static $client;
 
   /**
